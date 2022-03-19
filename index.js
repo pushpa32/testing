91a9d1ee62766771edf6463cda1db161a74dd754
Pushpa <pushpachhetri5@gmail.com>
2022-03-19 11:27:09 +0530

    auto2 commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Its me");
+  res.send("Hello Everuone");
 });
 
 app.listen(process.env.PORT || 3000, function () {
