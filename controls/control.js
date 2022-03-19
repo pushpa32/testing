356a64f109edb33cc4778e17779b4efa388f4a1d
Pushpa <pushpachhetri5@gmail.com>
2022-03-19 21:45:06 +0530

    create schema and save data in database

--- controls/control.js
+++ controls/control.js
@@ -1,6 +1,30 @@
+const res = require("express/lib/response");
+const mongoose = require("mongoose");
+const student = require("../model/student");
+const Student = require("../model/student");
+
 /* REGISTRATION */
 const register = (req, res) => {
   console.log(req.body);
+
+  const student = new Student({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    password: req.body.password,
+  });
+
+  student
+    .save()
+    .then((result) => {
+      console.log("result");
+      res.send("saved");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send("Error");
+    });
 };
 
 module.exports = register;
---
+++ model/student.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const stdSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: Number, required: true },
+  password: String,
+});
+
+module.exports = mongoose.model("Student", stdSchema);
