f8edf79b5e7d6c388a1a9805a050890e1c33f058
Pushpa <pushpachhetri5@gmail.com>
2022-03-19 22:14:40 +0530

    list the data from the database

--- router.js/routes.js
+++ router.js/routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const register = require("../controls/control");
+const Student = require("../model/student");
 
 const path = "./public";
-/* ROUTER
-   Register
-   Login
+/* 
+    ROUTER:
+      Register
+      Login
  */
 router.get("/register", (req, res) => {
   res.sendFile("register.html", { root: path });
@@ -16,6 +18,15 @@ router.get("/login", (req, res) => {
 });
 
 router.post("/register", register);
+router.get("/studentlist", (req, res) => {
+  Student.find()
+    .then((result) => {
+      res.status(200).json({ Student_Info: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 /* router.get("/", (req, res) => {
   res.sendFile("index.html", { root: path });
